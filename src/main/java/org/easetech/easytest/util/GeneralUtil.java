3c1220d948e85bb2b0d6c513a56d018c8a5d89d7
ravi-polampelli <ravi.polampelli@gmail.com>
2012-10-30 12:00:42 +0000

    Update src/main/java/org/easetech/easytest/util/GeneralUtil.java

--- src/main/java/org/easetech/easytest/util/GeneralUtil.java
+++ src/main/java/org/easetech/easytest/util/GeneralUtil.java
@@ -13,6 +13,7 @@ import java.util.Date;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.sql.Timestamp;
 
 /**
  * This class contains common utils
